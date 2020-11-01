306bb5d3b2ea8b7f6ba2b942d75f27a0840c5389
haschkan <62624041+haschkan@users.noreply.github.com>
2020-11-01 14:05:55 +0100

    login page created and maven up to date

--- src/test/java/com/libraryCT/pages/LoginPage.java
+++ src/test/java/com/libraryCT/pages/LoginPage.java
@@ -1,4 +1,36 @@
 package com.libraryCT.pages;
 
+import com.libraryCT.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
 public class LoginPage {
+
+    public LoginPage(){ PageFactory.initElements(Driver.get(), this); }
+
+    @FindBy(id = "inputEmail")
+    public WebElement emailInput;
+
+    @FindBy(id = "inputPassword")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//*[@type=\"submit\"]")
+    public WebElement submitButton;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
