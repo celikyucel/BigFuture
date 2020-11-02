82cc77481543fe6735315131cc5453ef8bc8ddbe
tiger1911 <mehmetatas@yahoo.com>
2020-11-02 12:52:33 +0100

    Locators added to BooksPage. BooksTable.feature file added

--- src/test/java/com/libraryCT/pages/BooksPage.java
+++ src/test/java/com/libraryCT/pages/BooksPage.java
@@ -1,4 +1,29 @@
 package com.libraryCT.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class BooksPage {
+
+    @FindBy(css = "#book_categories")
+    public WebElement bookCategoriesDropdown;
+
+    @FindBy(css = "select[name='tbl_books_length']")
+    public WebElement showRecordsDropdown;
+
+    @FindBy(css = "input[type='search']")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//table/thead/tr/th")
+    public List<WebElement> tableColumnNames;
+
+    @FindBy(xpath = "//span[text()='Books']")
+    public WebElement booksTabButton;
+
+
+
+
+
 }
