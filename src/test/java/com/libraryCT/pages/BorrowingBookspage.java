f0895dad0800bc69d0d8b90ef865e275713f22d8
yusuftml <yusuftemel355@gmail.com>
2020-11-03 17:03:50 +0100

    created Borrowing Books page

--- src/test/java/com/libraryCT/pages/BorrowingBookspage.java
+++ src/test/java/com/libraryCT/pages/BorrowingBookspage.java
@@ -1,4 +1,33 @@
 package com.libraryCT.pages;
 
+import com.libraryCT.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class BorrowingBookspage {
+
+    public BorrowingBookspage(){ PageFactory.initElements(Driver.get(),this); }
+
+    @FindBy (css = "a[href='#books']")
+    public WebElement booksOption;
+
+    @FindBy (css = " a[href='#borrowing-books']")
+    public WebElement borrowingBooksOption;
+
+    @FindBy (css = "#user_list")
+    public WebElement userList;
+
+
+
+    public static WebElement returnBook(String bookName){
+
+        String bookPath="//tbody//td[.='"+bookName+"']/../td[1]/a";
+
+       return  Driver.get().findElement(By.xpath(bookPath));
+
+    }
+
+
 }
