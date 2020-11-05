3f3c9463f23e51feb1c1a8e72a9ccbe683c8b133
zzambezii <zambezide@gmail.com>
2020-11-06 00:18:33 +0100

    after step deffs

--- src/test/java/com/libraryCT/step_definitions/LoginStepDefs.java
+++ src/test/java/com/libraryCT/step_definitions/LoginStepDefs.java
@@ -1,9 +1,31 @@
 package com.libraryCT.step_definitions;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginStepDefs {
+
+
+    @Given("the user navigates to login page")
+    public void the_user_navigates_to_login_page() {
+        String url = ConfigurationReader.get("url");
+        //WebDriver driver = Driver.get();
+        Driver.get().get(url);
+    }
+
+    @When("{string} enters {string} and {string}")
+    public void enters_and(String string, String string2, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user on  {string}")
+    public void user_on(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 
 
 
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,12 +1,13 @@
 Feature: Login functuonility
 
 @wip
-Scenario Outline:
-When user login as a "<role>"
+Scenario Outline: login as a user
+Given the user navigates to login page
+When "<role>" enters "<email>" and "<password>"
 Then user on  "<page>"
 
   Examples:
-| mail            | password |role      |page|
+| email            | password |role      |page|
 |student62@library |KbybTpIZ |student62|books.|
 |student63@library|Jg2orVoF  |student63|books.|
 |student64@library|pbdQAvkN  |student64|books.|
