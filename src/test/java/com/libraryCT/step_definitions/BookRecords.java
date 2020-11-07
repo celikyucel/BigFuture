46d4c07908f288af3d8bd7b67a5194ee6fdd9655
yusuftml <yusuftemel355@gmail.com>
2020-11-07 11:51:39 +0100

    created Book records branch

--- src/test/java/com/libraryCT/step_definitions/BookRecords.java
+++ src/test/java/com/libraryCT/step_definitions/BookRecords.java
@@ -1,6 +1,10 @@
 package com.libraryCT.step_definitions;
 
 import com.libraryCT.pages.BooksPage;
+import com.libraryCT.pages.LoginPage;
+import com.libraryCT.utilities.ConfigurationReader;
+import com.libraryCT.utilities.Driver;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.WebElement;
@@ -11,14 +15,30 @@ import java.util.List;
 
 public class BookRecords {
 
-    
+
+    @Given("the user is logged in2")
+    public void the_user_is_logged_in2() {
+
+        String url = ConfigurationReader.get("url");
+        //WebDriver driver = Driver.get();
+        Driver.get().get(url);
+
+        String username = ConfigurationReader.get("librarian_username");
+        String password = ConfigurationReader.get("librarian_password");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username,password);
+    }
+
     @Then("Verify default record is {int}")
     public void verify_default_record_is(Integer expectedRecord) {
+        new BooksPage().booksTabButton.click();
 
 
         Select select = new Select(new BooksPage().showRecordsDropdown);
 
         String defaultRecords = select.getFirstSelectedOption().getText();
+        System.out.println("defaultRecords = " + defaultRecords);
 
         Integer actualDefault = Integer.parseInt(defaultRecords);
 
@@ -38,6 +58,7 @@ public class BookRecords {
         for (WebElement recordOption : recordOptions) {
             actualRecords.add(recordOption.getText());
         }
+        System.out.println(actualRecords.toString());
 
         Assert.assertEquals(expectedOptions,actualRecords);
 
--- src/test/resources/features/BookRecords_EUG14-186.feature
+++ src/test/resources/features/BookRecords_EUG14-186.feature
@@ -1,9 +1,10 @@
 Feature:Librarian should be able to see book records on the user page
 
   @EUG14-186
+  @wip
   Scenario: Librarian should be able to see default book records
 
-    Given "librarian" is logged in
+    Given the user is logged in2
     Then Verify default record is 10
     And Verify book records is following options
       | 5   |
